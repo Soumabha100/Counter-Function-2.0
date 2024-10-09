09516bebec4300b3b7fdac4e269d931ebac2f3a8
Soumabha Majumder <soumabhamajumder@gmail.com>
2024-10-10 00:16:48 +0530

    CSS and JavaScript
    To commit styles and functions

---
+++ index.js
@@ -0,0 +1,19 @@
+const decraseBtn = document.getElementById("decraseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decraseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
